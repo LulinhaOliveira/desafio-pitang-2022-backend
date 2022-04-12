8678499615995c75313e83ebfa91c06a41ac377e
Lulinha Oliveira <lulinhaoliveirademelo@gmail.com>
2022-04-12 15:23:51 -0300

    Configuração do Express, Criação das Rotas e Escopo do Controller

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+import morgan from 'morgan';
+import express from 'express';
+import cors from 'cors';
+import SchedulingRouter from '../routes/SchedulingRouter.js';
+
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(SchedulingRouter);
+
+export default app;
