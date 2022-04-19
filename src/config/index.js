5c6b2aea1feb1542ddb6aa5cd0a08ea05fd58383
Luiz Oliveira <lulinhaoliveirademelo@gmail.com>
2022-04-19 19:47:36 -0300

    Implementação de Validação do Formato dos Dados

--- src/config/index.js
+++ src/config/index.js
@@ -2,11 +2,12 @@ import morgan from 'morgan';
 import express from 'express';
 import cors from 'cors';
 import SchedulingRouter from '../routes/SchedulingRouter.js';
+import { errorJSON } from '../middleware/schemas/schemasValidate.js';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
 app.use(SchedulingRouter);
-
+app.use(errorJSON)
 export  {app};
--- src/routes/SchedulingRouter.js
+++ src/routes/SchedulingRouter.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import {createValidator}  from "express-joi-validation"; 
 import SchedulingController from '../controllers/SchedulingController.js';
+import {querySchemaCreate, querySchemaUptaded} from '../middleware/schemas/schemasValidate.js';
 
 const schedulingController = new SchedulingController();
+const validator = createValidator({passError: true});
 
 const router = express.Router();
 
 router.get('/scheduling', schedulingController.getAll);
-router.post('/scheduling', schedulingController.store);
-router.patch('/scheduling/:id', schedulingController.uptadedStatus);
+router.post('/scheduling',validator.body(querySchemaCreate), schedulingController.store);
+router.patch('/scheduling/:id',validator.body(querySchemaUptaded) ,schedulingController.uptadedStatus);
 
 export default router;
