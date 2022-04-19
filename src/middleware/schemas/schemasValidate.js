6b724c3d78115703fa00c181c6f86aad4406e745
Luiz Oliveira <lulinhaoliveirademelo@gmail.com>
2022-04-19 20:01:41 -0300

    Aviso de Erro de Nome Vazio

--- src/middleware/schemas/schemasValidate.js
+++ src/middleware/schemas/schemasValidate.js
@@ -2,9 +2,10 @@ import Joi from "joi";
 
 const querySchemaCreate = Joi.object({
     name : Joi.string().required().messages({
-        'any.required': `Tenha Certeza de Está Colocando um Nome`
+        'any.required': `Tenha Certeza de Está Colocando um Nome`,
+        'string.empty' : 'O nome não deve ser vazio'
     }),
-    birth_date : Joi.date().format("unix").iso().required().messages({
+    birth_date : Joi.date().iso().required().messages({
         "date.format" : "Verifique o Formato da Data"
     }),
     date_time : Joi.string().pattern(new RegExp(/^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])T(00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9]).000Z$/)).messages({
