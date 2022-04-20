629c4980864b90b47d2d7d6820f82bfdffd3d002
Lulinha Oliveira <lulinhaoliveirademelo@gmail.com>
2022-04-20 13:12:43 -0300

    Teste de Validação dos Dados de Entrada

--- src/middleware/schemas/schemasValidate.js
+++ src/middleware/schemas/schemasValidate.js
@@ -10,11 +10,12 @@ for (let prop in Status) {
 
 const querySchemaCreate = Joi.object({
     name: Joi.string().required().messages({
-        'any.required': `Tenha Certeza de Está Colocando um Nome`,
-        'string.empty': 'O nome não deve ser vazio',
+        'any.required': `Name is required`,
+        'string.empty': 'The name must not be empty',
     }),
     birth_date: Joi.date().iso().required().messages({
-        'date.format': 'Verifique o Formato da Data',
+        'date.format': 'Enter a valid date',
+        'any.required': `Birth Date is required`,
     }),
     date_time: Joi.string()
         .pattern(
@@ -22,8 +23,11 @@ const querySchemaCreate = Joi.object({
                 /^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])T(00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9]).000Z$/
             )
         )
+        .required()
         .messages({
-            'string.pattern.base': 'Verifique o Formato da Data',
+            'string.pattern.base': 'Enter a valid date',
+            'string.empty': 'Enter a valid date',
+            'any.required': `Date and Time is required`,
         }),
 });
 
@@ -31,14 +35,14 @@ const querySchemaUptaded = Joi.object({
     status: Joi.valid(...arrayStatus)
         .required()
         .messages({
-            'any.only': 'Tenha Certeza de Está Colocando um Status Valido',
+            'any.only': 'Enter a valid Status (pending, attended, not_attended',
         }),
 });
 
 const errorJSON = (err, req, res, next) => {
     if (err && err.error && err.error.isJoi) {
         res.status(400).json({
-            message: err.error.toString(),
+            Message: err.error.toString().split(': ')[1],
         });
     } else {
         next(err);
